2f6562ab5702aaadb9b0cd512736d0269c8c6dc7
Marcos Maverick <marquinhopimpo@gmail.com>
2020-07-28 20:02:17 -0300

    Bispo

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -10,7 +10,7 @@ public class Pawn extends ChessPiece{
 	
 	private ChessMatch chessMatch;
 
-	public Pawn(Board board, Color color) {
+	public Pawn(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
 		this.chessMatch = chessMatch;
 		
